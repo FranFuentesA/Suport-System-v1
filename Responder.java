3f1682d52a974e98c0ad66e242896ca040865565
FranFuentesA <fran_fuentes20@hotmail.com>
2016-03-04 11:15:45 +0100

    Realizados cambios en la clase supportSystem y añadido contructor y generado metodo generateResponse()

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +10,22 @@
  */
 public class Responder
 {
+    private ArrayList<String> coleccionDeRespuestas;
+    private Random aleatorio;
+        
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        coleccionDeRespuestas = new ArrayList<String>();
+        aleatorio= new Random(); 
+        
+        coleccionDeRespuestas.add("Bienvenido al servicio de asistencia");
+        coleccionDeRespuestas.add("Indique que necesita resolver, por favor");
+        coleccionDeRespuestas.add("Incorrecto");
+        coleccionDeRespuestas.add("Correcto");
+        coleccionDeRespuestas.add("Espero que su incidencia este resuelta, hasta pronto");
     }
 
     /**
@@ -20,6 +34,6 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+         return coleccionDeRespuestas.get(aleatorio.nextInt(coleccionDeRespuestas.size()));
     }
 }
